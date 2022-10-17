133f883a4559434a76d0864036598a8243a4e9b7
wedul <wedul.chul@gmail.com>
2022-10-17 09:46:14 +0900

    firsot commit

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import type {Node} from 'react';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>My First React Native</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 30,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
